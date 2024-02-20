6c409f9867b827cbf07db4b191b455dc346c3dca
cristian <cdegracia@tribalyte.com>
2024-02-20 13:09:10 +0100

    add workout resume

--- src/app/workouts/_components/workout-details.tsx
+++ src/app/workouts/_components/workout-details.tsx
@@ -8,96 +8,39 @@ import { Badge } from "@components/ui/badge";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@components/ui/card";
+import { Separator } from "@components/ui/separator";
+import {
+  ArrowBigDown,
+  ArrowBigUp,
+  CheckCircle2,
+  CircleDot,
+  CircleEqual,
+  Equal,
+  Minus,
+  MoveRight,
+  PauseCircle,
+  Plus,
+  TrendingDown,
+  TrendingUp,
+  XCircle,
+} from "lucide-react";
+import WorkoutResume from "./workout-details/workout-resume";
 
 type Props = {
   workout: Workout;
 };
 
-const activities = [
-  {
-    name: "Pull Ups",
-    sets: 4,
-    reps: 12,
-    total: 48,
-  },
-  {
-    name: "Band Assisted Pull Ups",
-    sets: 2,
-    reps: 8,
-    total: 16,
-  },
-  {
-    name: "Straight Bar Dips",
-    sets: 4,
-    reps: 10,
-    total: 16,
-  },
-  {
-    name: "Rows",
-    sets: 3,
-    reps: 15,
-    total: 16,
-  },
-];
-
 export default function WorkoutDetails({ workout }: Props) {
   return (
-    <Tabs defaultValue="account" className="w-full">
+    <Tabs defaultValue="resume" className="w-full">
       <TabsList className="grid w-full grid-cols-4">
         <TabsTrigger value="resume">Resume</TabsTrigger>
+        <TabsTrigger value="progression">Progression</TabsTrigger>
         <TabsTrigger value="exercises">Exercises</TabsTrigger>
         <TabsTrigger value="resources">Resources</TabsTrigger>
-        <TabsTrigger value="progression">Progression</TabsTrigger>
       </TabsList>
       <TabsContent value="resume">
-        <Card className="min-h-[650px]">
-          <CardHeader>
-            <CardTitle>Resume</CardTitle>
-            <CardDescription>Sum up of your workout will be displayed here in detail.</CardDescription>
-          </CardHeader>
-          <CardContent className="flex gap-16">
-            <div className="w-1/3">
-              <h4 className="text-lg font-semibold mb-2">Description</h4>
-              <p className="text-sm mb-6">{workout.description}</p>
-
-              <h4 className="text-lg font-semibold mb-2">Difficulty</h4>
-              <Badge className="mb-6">{workout.difficulty.name}</Badge>
-
-              <h4 className="text-lg font-semibold mb-2">Muscles</h4>
-              {workout.muscles.map(({ name }, idx) => (
-                <Badge key={idx} variant="outline" className="me-1">
-                  {name}
-                </Badge>
-              ))}
-            </div>
-            <div className="w-2/3">
-              <h4 className="scroll-m-20 text-lg font-semibold tracking-tight mb-4">Exercises</h4>
-              <div className="rounded-md border">
-                <Table>
-                  {/* <TableCaption>List of sets and reps.</TableCaption> */}
-                  <TableHeader>
-                    <TableRow>
-                      <TableHead>Name</TableHead>
-                      <TableHead className="w-[50px]">Sets</TableHead>
-                      <TableHead className="w-[50px]">Reps</TableHead>
-                      <TableHead>Total</TableHead>
-                    </TableRow>
-                  </TableHeader>
-                  <TableBody>
-                    {activities.map((activity) => (
-                      <TableRow key={activity.name}>
-                        <TableCell className="font-medium">{activity.name}</TableCell>
-                        <TableCell>{activity.sets}</TableCell>
-                        <TableCell>{activity.reps}</TableCell>
-                        <TableCell>{activity.sets * activity.reps}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
+        <WorkoutResume workout={workout} />
       </TabsContent>
       <TabsContent value="exercises">
         <Card>
---
+++ src/app/workouts/_components/workout-details/workout-resume.tsx
@@ -0,0 +1,105 @@
+import { Badge } from "@components/ui/badge";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@components/ui/card";
+import { Separator } from "@components/ui/separator";
+import { TableHeader, TableRow, TableHead, TableBody, TableCell, Table } from "@components/ui/table";
+import { Workout } from "@typings/entities";
+import { CheckCircle2, XCircle, PauseCircle } from "lucide-react";
+
+type Props = {
+  workout: Workout;
+};
+
+const activities = [
+  {
+    name: "Pull Ups",
+    sets: 4,
+    reps: 12,
+    total: 48,
+    improve: "+",
+  },
+  {
+    name: "Band Assisted Pull Ups",
+    sets: 2,
+    reps: 8,
+    total: 16,
+    improve: "+",
+  },
+  {
+    name: "Straight Bar Dips",
+    sets: 4,
+    reps: 10,
+    total: 16,
+
+    improve: "=",
+  },
+  {
+    name: "Rows",
+    sets: 3,
+    reps: 15,
+    total: 16,
+    improve: "+",
+  },
+];
+
+export default function WorkoutResume({ workout }: Props) {
+  return (
+    <Card className="min-h-[400px]">
+      <CardHeader>
+        <CardTitle>Resume</CardTitle>
+        <CardDescription>Sum up of your workout will be displayed here in detail.</CardDescription>
+      </CardHeader>
+      <Separator className="mb-8" />
+      <CardContent className="flex gap-16">
+        <div className="w-1/3">
+          <h4 className="text-lg font-semibold mb-2">Description</h4>
+          <p className="text-sm mb-6">{workout.description}</p>
+
+          <h4 className="text-lg font-semibold mb-2">Difficulty</h4>
+          <Badge className="mb-6">{workout.difficulty.name}</Badge>
+
+          <h4 className="text-lg font-semibold mb-2">Muscles</h4>
+          {workout.muscles.map(({ name }, idx) => (
+            <Badge key={idx} variant="outline" className="me-1">
+              {name}
+            </Badge>
+          ))}
+        </div>
+        <div className="w-2/3">
+          <h4 className="scroll-m-20 text-lg font-semibold tracking-tight mb-2">Last Workout</h4>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Name</TableHead>
+                  <TableHead className="w-[50px]">Sets</TableHead>
+                  <TableHead className="w-[50px]">Reps</TableHead>
+                  <TableHead className="w-[50px]">Total</TableHead>
+                  <TableHead className="w-[50px]">Improve</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {activities.map((activity) => (
+                  <TableRow key={activity.name}>
+                    <TableCell className="px-4">{activity.name}</TableCell>
+                    <TableCell className="px-4 text-center">{activity.sets}</TableCell>
+                    <TableCell className="px-4 text-center">{activity.reps}</TableCell>
+                    <TableCell className="px-4 text-center">{activity.sets * activity.reps}</TableCell>
+                    <TableCell className="px-4 flex justify-center items-center">
+                      {activity.improve === "+" ? (
+                        <CheckCircle2 strokeWidth={2} color="#2563eb" className="w6 h-6" />
+                      ) : activity.improve === "-" ? (
+                        <XCircle color="#e11d48" strokeWidth={2} className="w6 h-6" />
+                      ) : (
+                        <PauseCircle strokeWidth={2} color="#94a3b8" className="w-6 h-6" />
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}

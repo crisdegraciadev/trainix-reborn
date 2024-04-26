693dc512c56a5a6b687665a9607a3484f78522bb
crisdegraciadev <crisdegraciadev@gmail.com>
2024-04-26 09:05:15 +0200

    ci: add cypress cloud support

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "trainix-reborn-cy",
   e2e: {
     baseUrl: "http://localhost:3000",
     viewportWidth: 1920,
     viewportHeight: 1080,
+    retries: {
+      runMode: 4,
+      openMode: 0,
+    },
   },
 });

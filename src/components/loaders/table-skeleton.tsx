198deeb6f571e8097ca582762844b5de7105f2ba
crisdegraciadev <crisdegraciadev@gmail.com>
2024-04-30 14:49:06 +0200

    fix: modify table skeleton loader

--- src/components/loaders/table-skeleton.tsx
+++ src/components/loaders/table-skeleton.tsx
@@ -4,7 +4,11 @@ export default function TableSkeleton() {
   return (
     <div className="w-full flex flex-col space-y-2" data-cy="loading-table">
       <div className="w-full flex justify-between">
-        <Skeleton className="h-8 w-64" />
+        <div className="flex gap-1">
+          <Skeleton className="h-8 w-64" />
+          <Skeleton className="h-8 w-[105px]" />
+          <Skeleton className="h-8 w-[105px]" />
+        </div>
         <Skeleton className="h-8 w-[98px]" />
       </div>
       <Skeleton className="h-96 w-full rounded-md" />
